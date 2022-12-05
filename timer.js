20643d79d7be657335184cf256a68a683843cb25
joaoomartins <joao62806@gmail.com>
2022-12-05 22:50:54 +0000

    Updates, remove alguns ficheiros inuteis :o, timer a funfar e o resto ta bom xD

--- timer.js
+++ timer.js
@@ -1,33 +1,32 @@
-var countUpDate = new Date("Jul, 23 2022 22:30:00").getTime();
-    
-      // +1 segundo
-      var x = setInterval(function() {
-    
-      // Data atual
-      var now = new Date().getTime();
-
-      var distance = now - countUpDate;
-
-        /*
-        var secondsInADay = 60 * 60 * 1000 * 24, secondsInAHour = 60 * 60 * 1000;
-    
-        days = Math.floor(timeDifference / (secondsInADay) * 1);
-        years = Math.floor(days / 365);
-        if (years > 1){ days = days - (years * 365) }
-        hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
-        mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
-        secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
-        */
-
+window.onload = function() {
+    // Month Day, Year Hour:Minute:Second, id-of-element-container
+    countUpFromTime("July 23, 2022 23:30:00", 'countup1'); // ****** Change this line!
+  };
+  function countUpFromTime(countFrom, id) {
+    countFrom = new Date(countFrom).getTime();
+    var now = new Date(),
+        countFrom = new Date(countFrom),
+        timeDifference = (now - countFrom);
       
-      //var years = Math.floor(distance /(1000 * 60 * 60 * 60 * 60) / 2);
-      var months = Math.floor(distance / (1000 * 60 * 60 * 60));
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-      document.getElementById("demo").innerHTML =/*years + " Anos " +*/ months + " Meses " + days + " Dias " + hours + " Horas "
-      + minutes + " Minutos " + seconds + " Segundos ";
+    var secondsInADay = 60 * 60 * 1000 * 24,
+        secondsInAHour = 60 * 60 * 1000;
       
-    }, 1000);
+    days = Math.floor(timeDifference / (secondsInADay) * 1);
+    years = Math.floor(days / 365);
+    if (years > 1){ days = days - (years * 365) }
+    months = Math.floor(days / 30);
+    hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+    mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+    secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+  
+    var idEl = document.getElementById(id);
+    idEl.getElementsByClassName('years')[0].innerHTML = years;
+    idEl.getElementsByClassName('months')[0].innerHTML = months;
+    idEl.getElementsByClassName('days')[0].innerHTML = days;
+    idEl.getElementsByClassName('hours')[0].innerHTML = hours;
+    idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
+    idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+  
+    clearTimeout(countUpFromTime.interval);
+    countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
+  }
