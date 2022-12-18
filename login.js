ab3869df47686396e6c07673c962aed17a9125ad
joaoomartins <joao62806@gmail.com>
2022-12-18 21:19:16 +0000

    add photos, login form and more coisas tantas

---
+++ login.js
@@ -0,0 +1,25 @@
+var frase = 'always' || 'Always',
+    data = '23/07/2022',
+    senha = '749123',
+    nome = document.getElementById("nome").value;
+  
+    function modifyState() {
+      let stateObj = { id: "100" };
+      window.history.replaceState(stateObj,"timer.html", "/timer.html");
+  }
+  
+  
+  function validar() {
+    if ((document.getElementById("frase").value == frase) & (document.getElementById("data").value == data) & (document.getElementById("senha").value == senha)) {
+      alert("Boa, bem-vindo(a), " + nome);
+      modifyState();
+      window.location.reload();
+      window.location.reload(false);      
+    }
+    else {
+      alert("Erro!");
+    }
+  }
+  //PARA POSSIVEIS ERROS - USAR !
+  //https://acervolima.com/como-substituir-url-simples-por-link-usando-javascript/
+  //https://acervolima.com/como-modificar-o-url-sem-recarregar-a-pagina-usando-javascript/ e chamar o window.reload();
